8c4c30e1763419720601c6a04389dcec1521875f
Arjun Bhujel <arjunbhujel600@gmail.com>
2024-04-11 11:25:27 +0545

    added copyright section

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -1,31 +1,35 @@
 import "@/styles/footer.css"
+import Copyright from "../ui/copyright"
 
 export default function Footer() {
   return (
-    <div className="footer">
-      <div className="contact">
-        <h2>CONTACT US</h2>
-        <div className="contact_info">
-          <div className="contact_info-description">
-            <p>We are a registered non-profit organization</p>
-            <p>Registration No - 125145</p>
+    <>
+      <div className="footer">
+        <div className="contact">
+          <h2>CONTACT US</h2>
+          <div className="contact_info">
+            <div className="contact_info-description">
+              <p>We are a registered non-profit organization</p>
+              <p>Registration No - 125145</p>
+            </div>
+            <div className="contact_info-location">
+              <p>Itahari-20, Sunsari, Tarahara</p>
+              <p>Phone No. 9815465254</p>
+            </div>
           </div>
-          <div className="contact_info-location">
-            <p>Itahari-20, Sunsari, Tarahara</p>
-            <p>Phone No. 9815465254</p>
-          </div>
+          <h2 className="heading_email">tarahararun@gmail.com</h2>
         </div>
-        <h2 className="heading_email">tarahararun@gmail.com</h2>
+        <div className="map">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d807.0997721494327!2d87.27455867482226!3d26.704140051638063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ef6bbc5edeb589%3A0x14a4bd2956a49202!2sManakamana%20digital%20Studio%20%26%20Video%20shooting%20centre!5e0!3m2!1sen!2snp!4v1712807785567!5m2!1sen!2snp"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            className="iframe_map"
+          ></iframe>
+        </div>
       </div>
-      <div className="map">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d807.0997721494327!2d87.27455867482226!3d26.704140051638063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ef6bbc5edeb589%3A0x14a4bd2956a49202!2sManakamana%20digital%20Studio%20%26%20Video%20shooting%20centre!5e0!3m2!1sen!2snp!4v1712807785567!5m2!1sen!2snp"
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-          className="iframe_map"
-        ></iframe>
-      </div>
-    </div>
+      <Copyright />
+    </>
   )
 }
---
+++ components/ui/copyright.jsx
@@ -0,0 +1,21 @@
+import "@/components/ui/styles/copyright.css"
+
+export default function Copyright() {
+  return (
+    <div className="copyright">
+      <p className="copyright_info">© 2024 Tarahara Run.</p>
+      <p className="copyright_developer">
+        Made with ❤ by{" "}
+        <a
+          href="https://arjunbhujel.com.np/"
+          rel="noreferrer"
+          target="_blank"
+          passHref={true}
+          className="developer_link"
+        >
+          Arjun Bhujel
+        </a>
+      </p>
+    </div>
+  )
+}
