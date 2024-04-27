6ce47622b90ea5a34fb52331c6248a99abef46cd
Arjun Bhujel <arjunbhujel600@gmail.com>
2024-04-27 18:32:52 +0545

    added Weekly run banner

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
+import WeeklyRun from "@/components/ui/weeklyRun";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
+      <WeeklyRun />
     </main>
   );
 }
--- components/ui/weeklyRun.jsx
+++ components/ui/weeklyRun.jsx
@@ -1,9 +1,20 @@
+import Image from "next/image"
+
 import "@/components/ui/styles/weeklyRun.css"
+import WeeklyRunPhoto from "@/public/image/home/Photo.png"
 
 export default function WeeklyRun() {
   return (
-    <div>
-      <h1>this is weeklyRun</h1>
+    <div className="weeklyRun">
+      <h2 className="weeklyRun_heading">WE ORGANIZE WEEKLY RUN</h2>
+      <p className="weeklyRun_text">EVERY SATURDAY</p>
+      <p className="weeklyRun_text">TIME - 6:30 PM</p>
+      <p className="weeklyRun_text">LOCATION - PANIPIYA, TARAHARA</p>
+      <Image
+        className="weeklyRun_image"
+        src={WeeklyRunPhoto}
+        alt="Weekly Run Group Photo"
+      />
     </div>
   )
 }
